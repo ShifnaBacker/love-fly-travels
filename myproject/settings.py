fb74180d2a0787c29ed572b9b6ba388053134ce4
Shifna <shifnanilu@gmail.com>
2024-08-11 12:00:23 +0530

    change in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-&d=^o-334dwai@s42rodfh0s^$i0jl=cntu-iem%^ojs^(kjpz'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["103.235.106.106", "127.0.0.1", "localhost", "love-fly-travels.onrender.com"]
 
